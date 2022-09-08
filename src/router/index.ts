40eaa9c33225f84a80b27cfed0135937a185831d
ZhiYong <zhiyong.wu@airudder.com>
2022-09-08 17:25:46 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/index.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/about.vue')
+    },
+    {
+        path: '/404',
+        name: '404',
+        component: ()=> import('../views/[...404].vue'),
+      },
+  ]
+})
+
+export default router
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  // darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
